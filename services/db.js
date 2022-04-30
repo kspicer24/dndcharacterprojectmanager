c39a0d8fbf407307483a761e0b5f38ca80671d00
mariaclarabez <mariaclarabez6@gmail.com>
2022-04-30 18:28:47 -0400

    close unused connections

--- services/db.js
+++ services/db.js
@@ -7,6 +7,7 @@ async function query(sql, params) {
     console.log(sql);
   const connection = await mysql.createConnection(config.db);
   const [results, ] = await connection.execute(sql, params);
+  connection.end();
   return results;
 }
 
